59e3640c193ae80a2a7416c6192edc4102492dff
[carlos martinez] <theycallhellp@gmail.com>
2022-11-29 11:27:21 -0800

    attempted sw saving

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {GenerateSW} = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+
 
 module.exports = () => {
   return {
@@ -11,17 +15,51 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Contact Cards'
+      }),
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'Contact Card App',
+        short_name: 'Contact Cards',
+        description: 'Make your contact cards here',
+        start_url: './',
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      })
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ]
     }
   };
 };
